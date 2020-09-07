4c15013ccff1036ff47bf8d0173c592d61a1de1e
oreon <jag@jpc>
2020-09-07 17:48:47 +0300

    added decode

--- encode.py
+++ encode.py
@@ -1,4 +1,5 @@
 
+codes = {'a': '1', 'e': '2', 'i': '3', 'o': '4', 'u': '5'}
 
 def encode(msg):
     '''
@@ -13,6 +14,23 @@ def encode(msg):
 
     return msg
 
+def decode(msg):
+    '''
+    a method to convert vowels into letters 
+    assumes the sentence is already in small case 
+    '''
+    if msg == None:
+        return None
+   
+    for vowel, code in codes.items():
+        msg = msg.replace(code, vowel)
+
+    return msg
+
 if __name__ == "__main__":
+    print (codes.items())
+    print (codes.keys())
     print ( encode('this is a message') )
+    print ( decode('th3s 3s 1 m2ss1g2') )
+
     print (encode(''))
--- test_encode.py
+++ test_encode.py
@@ -1,9 +1,16 @@
 #import pytest
-from .encode import encode
+from .encode import encode, decode
 
-def test_file1_method1():
+def test_encode():
 	assert encode('this is a message') == 'th3s 3s 1 m2ss1g2', \
-		"encoding regular string failed"
+		"encoding valid string failed"
 	
-def test_file1_method2():
+def test_encode_none():
 	assert encode(None) == None,"encoding of None failed" 
+
+def test_decode():
+	assert decode('th3s 3s 1 m2ss1g2') == 'this is a message', \
+		"decoding valid string failed"
+
+def test_decode_none():
+	assert decode(None) == None,"encoding of None failed" 
